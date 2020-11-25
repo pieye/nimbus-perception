e1c899bbe843b4cf4add31db8114047492cd5407
Bjarne Johannsen <bjarne.johannsen@protonmail.com>
2020-11-25 18:30:29 +0100

    functional, but problem with the coordinate frame

--- nimbus_agv_stop/src/main.cpp
+++ nimbus_agv_stop/src/main.cpp
@@ -1,11 +1,15 @@
+//ROS
 #include <ros/ros.h>
-// PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
+#include <std_msgs/Int8.h>
+#include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
+
+// PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include "pcl_ros/point_cloud.h"
-
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
@@ -13,31 +17,31 @@
 #include <pcl/filters/crop_hull.h>
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/extract_indices.h>
-
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/visualization/pcl_visualizer.h>
-
 #include <pcl_ros/point_cloud.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/point_cloud.h>
-
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/conditional_removal.h>
-
 #include <pcl/filters/filter.h>
 #include <pcl/filters/random_sample.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+//Misc
 #include <chrono> 
-
 
 typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
 ros::Publisher pub_roi;
 ros::Publisher pub_voxel;
 ros::Publisher pub_outlier;
 ros::Publisher pub_ground;
+
+ros::Publisher vis_pub;
+ros::Publisher warn_pub;
+
 
 void cloud_cb (const PointCloud::ConstPtr& cloud)
 {
@@ -100,6 +104,69 @@ void cloud_cb (const PointCloud::ConstPtr& cloud)
     ROS_INFO_STREAM("inliers: " << inliers->indices.size());
     ROS_INFO_STREAM("Removed Points: " << cloud->points.size()-cloud_filtered_ground->points.size() << " Points. In time: " << elapsed.count());
 
+    std_msgs::Int8 warn_msg;
+    
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "nimbus";
+    marker.header.stamp = ros::Time();
+    marker.ns = "nimbus_agv_emergency";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::CUBE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 0.5;
+    marker.scale.y = 1;
+    marker.scale.z = 2;
+      
+    //Determine closest point and show warning and emergency
+    if(cloud_filtered_ground->points.size() > 0){
+      float closest_point = 99;
+      int closest_point_index = 0; 
+      for(int i = 0; i < cloud_filtered_ground->points.size(); i++){
+        if(closest_point > cloud_filtered_ground->points[i].y)
+          closest_point = cloud_filtered_ground->points[i].y;
+      }
+      marker.pose.position.x = cloud_filtered_ground->points[closest_point_index].x;
+      marker.pose.position.y = cloud_filtered_ground->points[closest_point_index].y;
+      marker.pose.position.z = 1;
+      if(closest_point < 1){
+        marker.color.a = 0.7; 
+        marker.color.r = 1.0;
+        marker.color.g = 0.0;
+        marker.color.b = 0.0;
+        warn_msg.data = 2;
+      }
+      else if(closest_point < 2){
+        marker.color.a = 0.5; 
+        marker.color.r = 1.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+        warn_msg.data = 1;
+      }
+      else{
+        marker.color.a = 0.0; 
+        marker.color.r = 0.0;
+        marker.color.g = 0.0;
+        marker.color.b = 0.0;
+        warn_msg.data = 0;
+      }
+    }
+    else{
+      marker.color.a = 0.0; 
+      marker.color.r = 0.0;
+      marker.color.g = 0.0;
+      marker.color.b = 0.0; 
+      warn_msg.data = 0;
+    }
+    vis_pub.publish(marker);
+    warn_pub.publish(warn_msg);
+
     //publish the data.
     pub_roi.publish(cloud_filtered);
     pub_voxel.publish(cloud_filtered_voxel);
@@ -118,6 +185,9 @@ int main (int argc, char** argv)
   pub_voxel    = nh.advertise<pcl::PointCloud<pcl::PointXYZI>>("voxelized", 1);
   pub_outlier  = nh.advertise<pcl::PointCloud<pcl::PointXYZI>>("outlier", 1);
   pub_ground   = nh.advertise<pcl::PointCloud<pcl::PointXYZI>>("ground", 1);
+
+  vis_pub = nh.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
+  warn_pub = nh.advertise<std_msgs::Int8>( "obstacle", 0 );
 
   // Create a ROS subscriber for the input point cloud
   ros::Subscriber sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZI>>("/nimbus/pointcloud", 1, cloud_cb);
